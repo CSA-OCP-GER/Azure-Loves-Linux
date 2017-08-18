916010747168d9f0126898ffb14768a9f3f95e22
erjosito <erjosito@users.noreply.github.com>
2017-08-18 14:44:18 +0200

    Create ubuntuConfig.sh

---
+++ ubuntuConfig.sh
@@ -0,0 +1,10 @@
+# Install Apache and PHP
+sudo apt-get update
+sudo apt-get install apache2 -y
+sudo apt-get install php libapache2-mod-php php-mcrypt php-mysql -y
+sudo systemctl restart apache2
+
+# Delete default web site and download a new one
+sudo rm /var/www/html/index.html
+sudo apt-get install wget -you
+sudo wget https://raw.githubusercontent.com/CSA-OCP-GER/azure-loves-linux/master/index.php -P /var/www/html/
