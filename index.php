15cd065f5a2b7a09f4f2b95f1933cae41a09d113
erjosito <erjosito@users.noreply.github.com>
2017-08-18 14:48:40 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,32 +1,16 @@
 <html>
    <header>
-     <title>Network Virtual Appliance</title>
+     <title>Welcome in your new Linux machine</title>
    </header>
    <body>
      <h1>
-       Welcome to the Open Source Azure Networking Lab
+       Welcome to your new Linux VM
      </h1>
      <br>
+     <p>Here you have some information about this VM:<p>
      <?php
-       $hosts = array ("bing.com", "10.1.1.4");
-       $allReachable = true;
-       foreach ($hosts as $host) {
-         $result = exec ("ping -c 1 -W 1 " . $host . " 2>&1 | grep received");
-         $pos = strpos ($result, "1 received");
-         if ($pos === false) {
-           $allReachable = false;
-           break;
-         }
-       }
-       if ($allReachable === false) {
-         // Ping did not work
-         http_response_code (299);
-         print ("The target hosts do not seem to be all reachable (" . $host . ")\n");
-       } else {
-         // Ping did work
-         http_response_code (200);
-         print ("All target hosts seem to be reachable\n");
-       }
+       $publicIp = exec ("curl ifconfig.co");
+       print ("The public IP address this host uses to reach the public Internet is " . $host . "\n");
      ?>
    </body>
 </html>
